b1920f7dc29e1ca982c86112253286083b2d9877
Sergey Khroulev <sergey.hrulev@gmail.com>
2018-10-30 08:38:38 +0300

    first commit

---
+++ resources/views/templates/header.blade.php
@@ -0,0 +1,12 @@
+<div class="container">
+    <div class="row">
+        <nav class="nav justify-content-center">
+            <li class="nav-item">Главная</li>
+            <li class="nav-item active">Продукция</li>
+            <li class="nav-item">О компании</li>
+            <li class="nav-item">Проекты</li>
+            <li class="nav-item">Контакты</li>
+        </nav>
+
+    </div>
+</div>
